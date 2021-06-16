b6a1a9b188b000dac4789a64b0f986c923102241
Gustavo Napoli <gnkenshin@gmail.com>
2021-06-16 14:00:14 -0300

    prometheus e refatoração

--- proposta/src/main/java/br/com/zup/proposta/config/security/SecurityConfiguration.java
+++ proposta/src/main/java/br/com/zup/proposta/config/security/SecurityConfiguration.java
@@ -1,10 +1,14 @@
 package br.com.zup.proposta.config.security;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
 	@Override
@@ -14,7 +18,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 				.antMatchers(HttpMethod.POST, "/proposal/**").hasAuthority("Scope_propostas:write")
 				.antMatchers(HttpMethod.POST, "/card/**").hasAuthority("Scope_cartao:write")
 				.antMatchers(HttpMethod.GET, "/card/**").hasAuthority("Scope_cartao:read")
+				.antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
 				.anyRequest().authenticated())
 		.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
 	}
+
 }
